505e0be0115d7da0492b4cabee37cfcc1590f510
bchiu <bchiu@axon.com>
2023-10-29 21:20:52 +0700

    wire the sstable

--- internal/nexus.go
+++ internal/nexus.go
@@ -4,17 +4,24 @@ import (
 	"context"
 
 	pb "nexus/internal/interface"
+	"nexus/internal/storage"
 
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type NexusServer struct {
 	pb.UnimplementedNexusServer
-	storage map[string]string
+	storage *storage.SSTable
 }
 
 func CreateNexus() *NexusServer {
-	return &NexusServer{storage: map[string]string{}}
+	return &NexusServer{storage: storage.NewSSTable(&storage.SSTableConfig{
+		Directory:        "data",
+		FilePrefix:       "Nexus",
+		SegmentThreshold: 4 * 1024 * 1024,
+		MemtableMaxSize:  1024 * 1024,
+		UseHash:          true,
+	})}
 }
 
 func (s *NexusServer) Ping(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
@@ -22,10 +29,17 @@ func (s *NexusServer) Ping(context.Context, *emptypb.Empty) (*emptypb.Empty, err
 }
 
 func (s *NexusServer) Put(ctx context.Context, putRequest *pb.PutRequest) (*pb.PutResponse, error) {
-	s.storage[putRequest.Key] = putRequest.Value
+	err := s.storage.Insert(putRequest.Key, putRequest.Value)
+	if err != nil {
+		return nil, err
+	}
 	return &pb.PutResponse{Err: 0}, nil
 }
 
 func (s *NexusServer) Get(ctx context.Context, getRequest *pb.GetRequest) (*pb.GetResponse, error) {
-	return &pb.GetResponse{Err: 0, Key: getRequest.Key, Value: s.storage[getRequest.Key]}, nil
+	value, found := s.storage.Find(getRequest.GetKey())
+	if !found {
+		return &pb.GetResponse{Err: -1}, nil
+	}
+	return &pb.GetResponse{Err: 0, Key: getRequest.Key, Value: value}, nil
 }
